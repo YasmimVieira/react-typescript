1b5538ae39a55342784371e8b1428956c12dca50
Yasmim Barbosa Vieira <vieiray937@gmail.com>
2022-03-19 15:23:03 -0300

    chore: create new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import Button from "./components/Button";
+import Form from "./components/Form";
+import Lista from "./components/Lista/list";
+
 function App() {
   return (
     <div className="App">
-      Hello world
+      <Form />
+      <Lista />
     </div>
   );
 }
